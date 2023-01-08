466eabf1f36fd03c998deb02e9a59d8e506c2e8e
YOUNG <moxnox63@gmail.com>
2023-01-08 14:15:38 +0900

    [Shop] Listing product cards with map()

--- simple-shop/src/App.js
+++ simple-shop/src/App.js
@@ -1,11 +1,17 @@
 /* eslint-disable */
 
+import { useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './App.css';
+import data from './data.js';
+
+// 실제로는 서버에서 가져온 데이터를 변수에 담아서 사용
 
 // react bootstrap 외부 라이브러리
 // App.js 는 쇼핑몰 메인 페이지
 function App() {
+  let [shoes, setShoes] = useState(data); // data파일의 data를 가져와서 shoe라는 state로 사용
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -22,27 +28,52 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
+
+          {
+            shoes.map((a, i) => {
+              return (
+                <Card shoes={shoes[i]} i={i} />
+              )
+            })
+          }
+
+          {/* <Card shoes={shoes[0]} i={0} />
+          <Card shoes={shoes[1]} i={1} />
+          <Card shoes={shoes[2]} i={2} /> */}
+
+          {/* <div className="col-md-4">
             <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
+            <p>{shoes[0].price}</p>
           </div>
           <div className="col-md-4">
             <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content}</p>
+            <p>{shoes[2].price}</p>
+          </div> */}
         </div>
       </div>
-
-      <br />
     </div >
   );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ simple-shop/src/data.js
@@ -0,0 +1,25 @@
+// 제품 정보를 따로 담아둘 파일
+// 다른 파일의 자료를 사용하려면 import / export
+
+let data = [
+    {
+        id: 0,
+        title: "White and Black",
+        content: "Born in France",
+        price: 120000
+    },
+    {
+        id: 1,
+        title: "Red Knit",
+        content: "Born in Seoul",
+        price: 110000
+    },
+    {
+        id: 2,
+        title: "Grey Yordan",
+        content: "Born in the States",
+        price: 130000
+    }
+];
+
+export default data;
