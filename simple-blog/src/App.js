992fcbff8a0cd6e4487a0c8a01d1d51dc2c4ee6a
YOUNG <moxnox63@gmail.com>
2023-01-22 19:00:47 +0900

    [Blog] Navbar created

--- simple-blog/src/App.js
+++ simple-blog/src/App.js
@@ -1,41 +1,72 @@
-import React from "react";
-import { useState } from "react";
+import { useState } from 'react';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
   const onSubmit = () => {
-    console.log(title, body);
-  }
+    axios.post('http://localhost:3001/posts', {
+      title,
+      body
+    })
+  };
 
   return (
-    <div className="container">
-      <div className="mb-3">
-        <label className="form-label">Title</label>
-        <input
-          className="form-control"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Body</label>
-        <textarea
-          className="form-control"
-          value={body}
-          onChange={(e) => {
-            setBody(e.target.value);
-          }}
-          rows={20}
-        />
-      </div>
+    <Router>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+          <Link class="navbar-brand" to="/">Home</Link>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/blogs">Blogs</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-      <button
-        className="btn btn-primary"
-        onClick={onSubmit}>Post</button>
-    </div>
+      <Routes>
+        <Route path="/" element={"Home Page"} />
+        <Route path="/blogs" element={
+          <div className="container">
+            <h1>Create a Blog Post</h1>
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input
+                className="form-control"
+                value={title}
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Body</label>
+              <textarea
+                className="form-control"
+                value={body}
+                onChange={(event) => {
+                  setBody(event.target.value);
+                }}
+                rows="20"
+              />
+            </div>
+            <button
+              className="btn btn-primary"
+              onClick={onSubmit}
+            >
+              Post
+            </button>
+          </div>
+        } />
+      </Routes>
+    </Router >
+
   );
 }
 
