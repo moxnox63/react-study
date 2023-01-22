5efe4932dd67556450739353a9dfbd180a53bd2d
YOUNG <moxnox63@gmail.com>
2023-01-22 17:26:06 +0900

    [Blog] Initial settings for blog project

---
+++ simple-blog/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    console.log(title, body);
+  }
+
+  return (
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input
+          className="form-control"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea
+          className="form-control"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+          rows={20}
+        />
+      </div>
+
+      <button
+        className="btn btn-primary"
+        onClick={onSubmit}>Post</button>
+    </div>
+  );
+}
+
+export default App;
